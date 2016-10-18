eb812545c976fc79155de1eb72b1971c65d336af
Andrei Balmus <abalmus@tacitknowledge.com>
2016-10-18 15:30:16 +0300

    Changing view model and template

--- src/select-2.js
+++ src/select-2.js
@@ -1,6 +1,52 @@
+// Aurelia Framework specific functionality
+import {bindable, inject, customElement} from 'aurelia-framework';
+
+// Import JSPM modules we installed earlier
 import $ from 'jquery';
-import select2 from 'select2/select2';
 
-console.log($, select2);
+@customElement('select2') // Define the name of our custom element
+@inject(Element) // Inject the instance of this element
+export class Select2 {
+    @bindable name = null; // The name of our custom select
+    @bindable selected = false; // The default selected value
+    @bindable options = {}; // The label/option values
+    @bindable labelProp = 'label';
+    @bindable valueProp = 'value';
 
-export class Select2 {}
+    constructor(element) {
+        this.element = element;
+    }
+
+    // selectedChanged() {
+    //     $(this.element).find('select').trigger('change');
+    // }
+
+    // Once the Custom Element has its DOM instantiated and ready for binding
+    // to happenings within the DOM itself
+    attached() {
+        setTimeout(() => {
+            $(this.element).find('select')
+            .select2({dropdownAutoWidth: 'true', width: 'auto'})
+            .on('change', (event) => {
+                let changeEvent;
+
+                if (window.CustomEvent) {
+                    changeEvent = new CustomEvent('change', {
+                        detail: {
+                            value: event.target.value
+                        },
+                        bubbles: true
+                    });
+                } else {
+                    changeEvent = document.createEvent('CustomEvent');
+                    changeEvent.initCustomEvent('change', true, true, {
+                        detail: {
+                            value: event.target.value
+                        }
+                    });
+                }
+                this.element.dispatchEvent(changeEvent);
+            });
+        }, 0);
+    }
+}
